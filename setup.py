a644440c56857526a9615c43df262d08483aba05
DomnenkoB <papadoble151@gmail.com>
2020-04-14 19:00:20 +0300

    updated setup.py

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent"
     ],
-    packages=["hybrid_search_engine"],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
     python_requires=">=3.6"
 )
